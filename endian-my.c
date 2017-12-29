ce5fef56e0aeaa0778ad0dafc9aad8693a2eac7b
ilyamironow <miilig@yandex.ru>
2017-12-29 16:31:21 +0300

    correction

--- endian-my.c
+++ endian-my.c
@@ -3,15 +3,17 @@
 union data {
 	int i;
 	char c[sizeof(int)];
-} 
-data;
+};
 
 void main() {
+	union data data;
 	data.i = 1;
 	printf("Number : %d\n", data.i);
 	printf("Memory representation : %d %d %d %d\n", data.c[0], data.c[1], data.c[2], data.c[3]);
-	if (data.c[0] == 1)
+	if (data.c[0] == 1) {
 		printf("little-endian\n");
-	else
+		}
+	else {
 		printf("big-endian\n");
+		}
 }
